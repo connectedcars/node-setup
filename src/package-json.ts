bf4504965737481790c7542d0e213e360fad8410
Michael Storgaard <git@storgaard.io>
2019-10-17 16:40:36 +0200

    Added more verbose logging

--- src/package-json.ts
+++ src/package-json.ts
@@ -38,10 +38,19 @@ export async function updatePackageJson(
   const force = options.force ? true : false
   const verbose = options.verbose ? true : false
 
+  if (verbose) {
+    console.log(`  Started reading files`)
+  }
   const templatePackageJson = await readPackageJson(`${templatePath}/package.json`)
   const packageJson = await readPackageJson(`${target}/package.json`)
+  if (verbose) {
+    console.log(`  Finished reading files`)
+  }
 
   // Update devDependencies
+  if (verbose) {
+    console.log(`  Started updating "devDependencies"`)
+  }
   for (const dependency of Object.keys(templatePackageJson.devDependencies)) {
     if (templatePackagesIgnore.includes(dependency)) {
       continue
@@ -52,19 +61,46 @@ export async function updatePackageJson(
     }
   }
   packageJson.devDependencies = sortDependencies(packageJson.devDependencies)
+  if (verbose) {
+    console.log(`  Finished updating "devDependencies"`)
+  }
   // Update scripts
+  if (verbose) {
+    console.log(`  Started updating "scripts"`)
+  }
   for (const scriptName of Object.keys(templatePackageJson.scripts)) {
     if (force || !packageJson.scripts[scriptName]) {
       packageJson.scripts[scriptName] = templatePackageJson.scripts[scriptName]
     }
   }
+  if (verbose) {
+    console.log(`  Finished updating "scripts"`)
+  }
   // Update engines
+  if (verbose) {
+    console.log(`  Started updating "engines"`)
+  }
   packageJson.engines = templatePackageJson.engines
+  if (verbose) {
+    console.log(`  Finished updating "engines"`)
+  }
   // Remove old configs
   if (force) {
+    if (verbose) {
+      console.log(`  Started deleting "babel" and "jest"`)
+    }
     delete packageJson.babel
     delete packageJson.jest
+    if (verbose) {
+      console.log(`  Finished deleting "babel" and "jest"`)
+    }
   }
 
+  if (verbose) {
+    console.log(`  Started updating "package.json"`)
+  }
   await writeFileAtomic(`${target}/package.json`, JSON.stringify(packageJson, null, 2))
+  if (verbose) {
+    console.log(`  Finished updating "package.json"`)
+  }
 }
