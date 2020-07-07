16221cfd7cb2adc9c1a841577e58f3efcd2883c9
Troels Liebe Bentsen <tlb@nversion.dk>
2020-07-07 12:20:05 +0200

    Use type from function we are wrapping

--- src/fsutils.ts
+++ src/fsutils.ts
@@ -6,16 +6,10 @@ import util from 'util'
 
 const readFile = util.promisify(fs.readFile)
 
-interface CreateWriteStreamOptions {
-  encoding?: string
-  flags?: string
-  mode?: number
-}
-
 export async function writeFileAtomic(
   filePath: string,
   data: Buffer | string | Stream,
-  options?: CreateWriteStreamOptions
+  options?: Parameters<typeof fs.createWriteStream>[1]
 ): Promise<void> {
   const basePath = path.dirname(filePath)
   const fileName = path.basename(filePath)
